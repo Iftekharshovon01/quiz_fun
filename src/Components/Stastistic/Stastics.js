1dae48d07de7b1eef7e50b9ed806c8f8ebc256de
Iftekhar <iftekharshovonseu17@gmail.com>
2022-10-12 23:30:39 +0600

    graph done

--- src/Components/Stastistic/Stastics.js
+++ src/Components/Stastistic/Stastics.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { Bar, BarChart } from 'recharts';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Stastics = () => {
-    const data = useLoaderData();
-    console.log(data);
+    const [questions, setQuestions] = useState([])
+    useEffect(()=>{
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+        .then(data=> setQuestions(data.data.data))
+    })
     return (
         <div className='py-20 font-bold'>
-        <BarChart width={150} height={40} data={data}>
-            
-          <Bar dataKey="" fill="#8884d8" />
-       
-        </BarChart>
+            <BarChart width={1000} height={500} data={questions}>
+            <Bar dataKey="" fill="#8884d8" />
+            <XAxis dataKey='name'/>
+            <YAxis dataKey='number'/>
+            <Tooltip/>
+            </BarChart>
         </div>
     );
 };
